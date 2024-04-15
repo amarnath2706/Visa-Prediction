226e02a9b6166f16d8db013a36d0ccadfe976b3c
Amar <amarnath.arjsd@gmail.com>
2024-04-15 20:56:20 +0530

    exception code

--- demo.py
+++ demo.py
@@ -1,5 +1,12 @@
 from visa_prediction.logger import logging
 from visa_prediction.exception import visaException
+import sys
 
 
 logging.info('Welcome to our custom log')
+
+try:
+    a = 20/"2"
+
+except Exception as e:
+    raise visaException(e, sys) from e
